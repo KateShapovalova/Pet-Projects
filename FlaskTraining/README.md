02e8573b1651baaa471786a27ff6f96b75d07991
KateShapovalova <shapovalova.kate1997@gmail.com>
2022-01-05 20:42:47 +0200

    Add postgres db.

---
+++ FlaskTraining/README.md
@@ -0,0 +1,7 @@
+### Work with db:
+
+flask db init
+
+flask db migrate -m "NAME table"
+
+flask db upgrade
--- FlaskTraining/app.py
+++ FlaskTraining/app.py
@@ -1,10 +1,18 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from forms import LoginForm
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
+import models
 
 @app.route('/')
+@app.route('/index')
 def index():
     user = {'username': 'Kate'}
     posts = [
@@ -20,6 +28,15 @@ def index():
     title = 'Practice Home Page - Ichigo page '
 
     return render_template('index.html', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me {}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 
 if __name__ == '__main__':
